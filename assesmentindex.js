b84e339b44c7764b948fc55182e0ce475560dbd0
Rameesha Nelumi <rameeshanelumi11@gmail.com>
2025-11-26 19:07:38 +1100

    Chanage Done
    Removed

--- assesmentindex.js
+++ assesmentindex.js
@@ -15,13 +15,6 @@ module.exports = function(data, filename, mime, bom) {
         tempLink.href = blobURL;
         tempLink.setAttribute('download', filename);
 
-        // Safari thinks _blank anchor are pop ups. We only want to set _blank
-        // target if the browser does not support the HTML5 download attribute.
-        // This allows you to download files in desktop safari if pop up blocking
-        // is enabled.
-        if (typeof tempLink.download === 'undefined') {
-            tempLink.setAttribute('target', '_blank');
-        }
 
         document.body.appendChild(tempLink);
         tempLink.click();
